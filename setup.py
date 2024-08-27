1fb5d416436e1b2d56494ac4c2cab9008c6413bb
Brandon Amos <brandon.amos.cs@gmail.com>
2024-08-27 16:25:16 -0400

    bump to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mpc',
-    version='0.0.5',
+    version='0.0.6',
     description="A fast and differentiable MPC solver for PyTorch.",
     author='Brandon Amos',
     author_email='brandon.amos.cs@gmail.com',
